d7da8f340ef0e98b235ec66995830c621cd12bd6
Vincent Rahadian Utama <vincentrahadianutama>
2023-05-09 08:51:26 +0700

    Upload PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP Project</title>
+</head>
+
+<body>
+    <?php
+    //Step 1
+    echo "Belajar PHP - Project";
+    echo "\n";
+    //Step 2
+    $name = "Belajar PHP menggunakan var";
+    echo $name;
+    ?>
+</body>
+
+</html>
